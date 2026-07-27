c35c93141fd4d55e72161aa5055621ef83d01943
Peixiong Gao <peixiong.gao@compass.com>
2026-07-27 17:01:30 +0800

    refactor(graph): import safeDecodeURIComponent directly from util/lang

--- src/components/scripts/graph.inline.ts
+++ src/components/scripts/graph.inline.ts
@@ -6,16 +6,9 @@ import {
   simplifySlug,
   resolveBasePath,
 } from "@quartz-community/utils";
+import { safeDecodeURIComponent } from "../../util/lang";
 
 (function () {
-  function safeDecode(str) {
-    try {
-      return decodeURIComponent(str);
-    } catch {
-      return str;
-    }
-  }
-
   function getSlugFromUrl() {
     var slug = getFullSlugFromUrl();
     var base = getBasePath();
@@ -95,7 +88,7 @@ import {
     }
 
     async function renderGraph(graph, fullSlug, renderGeneration) {
-      var slug = safeDecode(simplifySlug(fullSlug));
+      var slug = safeDecodeURIComponent(simplifySlug(fullSlug));
       if (slug === "") slug = "index";
       var visited = getVisited();
       removeAllChildren(graph);
@@ -125,7 +118,7 @@ import {
         var dataRaw = await fetchData;
         data = new Map();
         for (var key in dataRaw) {
-          data.set(safeDecode(simplifySlug(key)), dataRaw[key]);
+          data.set(safeDecodeURIComponent(simplifySlug(key)), dataRaw[key]);
         }
       } catch (err) {
         console.error("[Graph] Error loading data:", err);
@@ -142,7 +135,7 @@ import {
       data.forEach(function (details, source) {
         var outgoing = details.links || [];
         for (var i = 0; i < outgoing.length; i++) {
-          var dest = safeDecode(simplifySlug(outgoing[i]));
+          var dest = safeDecodeURIComponent(simplifySlug(outgoing[i]));
           if (validLinks.has(dest)) {
             links.push({ source: source, target: dest });
           }
@@ -153,7 +146,7 @@ import {
           for (var i = 0; i < tags.length; i++) {
             var tag = tags[i];
             if (removeTags.indexOf(tag) === -1) {
-              var tagSlug = safeDecode(simplifySlug("tags/" + tag));
+              var tagSlug = safeDecodeURIComponent(simplifySlug("tags/" + tag));
               if (allTags.indexOf(tagSlug) === -1) {
                 allTags.push(tagSlug);
               }
@@ -202,7 +195,7 @@ import {
       var nodeMap = new Map();
       neighbourhood.forEach(function (url) {
         var isTag = url.startsWith("tags/");
-        var text = isTag ? "#" + url.substring(5) : data.get(url)?.title || safeDecode(url);
+        var text = isTag ? "#" + url.substring(5) : data.get(url)?.title || safeDecodeURIComponent(url);
         var nodeTags = isTag ? [] : data.get(url)?.tags || [];
         var node = {
           id: url,
